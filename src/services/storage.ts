09953c14037ec273a2740b4704ed92ebf4fff6a4
Lucio Tudisco <lucio.tudisco@gmail.com>
2025-08-31 16:37:44 +0100

    Update

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -6,8 +6,8 @@ class StorageService {
 
     constructor() {
         const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-        const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-        this.client = createClient(supabaseUrl, supabaseKey);
+        const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+        this.client = createClient(supabaseUrl, supabaseServiceKey);
     }
 
     async createTld(tldInfo: TLD): Promise<void> {
