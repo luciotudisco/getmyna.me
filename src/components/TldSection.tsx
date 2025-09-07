e4349e9dc8b6c37b2bab4a4e6386e23dc4854e54
Lucio Tudisco <lucio.tudisco@gmail.com>
2025-09-07 21:27:46 +0100

    Update

--- src/components/TldSection.tsx
+++ src/components/TldSection.tsx
@@ -6,15 +6,12 @@ interface TldSectionProps extends TLD {
     tld: string;
 }
 
-export default function TldSection({ tld, description, type }: TldSectionProps) {
+export default function TldSection({ tld, description }: TldSectionProps) {
     const wikipediaUrl = `https://en.wikipedia.org/wiki/.${tld}`;
     const tldDescription = description ?? 'No additional information is available for this TLD.';
     return (
         <p className="text-xs">
-            <span className="font-bold">
-                .{tld} ({type}):
-            </span>{' '}
-            {tldDescription}{' '}
+            <span className="font-bold">.{tld}:</span> {tldDescription}{' '}
             <a href={wikipediaUrl} target="_blank" rel="noopener noreferrer" className="text-blue-600 underline">
                 Learn more on Wikipedia
             </a>
