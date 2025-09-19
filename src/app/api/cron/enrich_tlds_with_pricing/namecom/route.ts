9c2ed1b449e21eb06a57ae8b6aff905422221762
luciotudisco <lucio.tudisco@gmail.com>
2025-09-19 21:30:54 +0100

    Clean up audit logs

---
+++ src/app/api/cron/enrich_tlds_with_pricing/namecom/route.ts
@@ -0,0 +1,65 @@
+import axios from 'axios';
+import { NextResponse } from 'next/server';
+import { tldRepository } from '@/services/tld-repository';
+import { Registrar, TLDPricing } from '@/models/tld';
+
+export const maxDuration = 300; // This function can run for a maximum of 5 minutes
+
+const NAMECOM_API_KEY = process.env.NAMECOM_API_KEY;
+const NAMECOM_PRICES_URL = `https://api.name.com/core/v1/tldpricing`;
+
+/**
+ * The response from the Name.com API for pricing information.
+ * See https://docs.name.com/coreapi/namecom.api/tld-pricing/tldpricelist for more details.
+ */
+interface NameComPricingResponse {
+    nextPage?: number;
+    pricing: {
+        tld: string;
+        registrationPrice: string;
+        renewalPrice: string;
+    }[];
+}
+
+/**
+ * Enrich TLDs with the pricing information from Name.com.
+ * This function fetches the TLDs from the database and enriches them with the pricing information from Name.com.
+ * It then updates the TLDs in the database with the new pricing information.
+ */
+export async function GET(): Promise<NextResponse> {
+    try {
+        console.log('Starting TLD pricing enrichment from Name.com ...');
+        const headers = { Authorization: `Basic ${NAMECOM_API_KEY}` };
+        let page = 1;
+        let hasMoreResults = true;
+        let MAX_PAGES = 100;
+        while (hasMoreResults) {
+            const response = await axios.get<NameComPricingResponse>(NAMECOM_PRICES_URL, { headers, params: { page } });
+            const pricingItems = response.data.pricing;
+            for (const pricingItem of pricingItems) {
+                const tld = pricingItem.tld;
+                console.log(`TLD ${tld} found in Name.com pricing`);
+                const tldInfo = await tldRepository.getTLD(tld);
+                if (!tldInfo) {
+                    console.log(`TLD ${tld} not found in database. Skipping...`);
+                    continue;
+                }
+                const tldPricing: TLDPricing = {
+                    registration: parseFloat(parseFloat(pricingItem.registrationPrice).toFixed(2)),
+                    renewal: parseFloat(parseFloat(pricingItem.renewalPrice).toFixed(2)),
+                    currency: 'USD',
+                };
+                const updatedPricing = { ...tldInfo?.pricing, [Registrar.NAMECOM]: tldPricing };
+                await tldRepository.updateTLD(tld, { pricing: updatedPricing });
+                console.log(`Updated ${tld} with Name.com pricing`);
+            }
+            hasMoreResults = response.data.nextPage !== null && page < MAX_PAGES;
+            page += 1;
+        }
+        console.log('TLD pricing enrichment from Name.com completed');
+        return NextResponse.json({ message: 'TLD pricing enrichment from Name.com completed successfully' });
+    } catch (error) {
+        console.error('Error during TLD pricing enrichment from Name.com:', error);
+        return NextResponse.json({ error: 'Failed to enrich TLD pricing from Name.com' }, { status: 500 });
+    }
+}
--- src/models/tld.ts
+++ src/models/tld.ts
@@ -16,6 +16,7 @@ export enum TLDType {
 export enum Registrar {
     DYNADOT = 'DYNADOT',
     GANDI = 'GANDI',
+    NAMECOM = 'NAMECOM',
     NAMESILO = 'NAMESILO',
     PORKBUN = 'PORKBUN',
 }
@@ -23,6 +24,7 @@ export enum Registrar {
 export const REGISTRARS_DOMAIN_SEARCH_URLS = {
     [Registrar.DYNADOT]: (domain: string) => `https://www.dynadot.com/domain/search?domain=${domain}`,
     [Registrar.GANDI]: (domain: string) => `https://shop.gandi.net/domain/suggest?search=${domain}`,
+    [Registrar.NAMECOM]: (domain: string) => `https://www.name.com/domain/search/${domain}`,
     [Registrar.NAMESILO]: (domain: string) => `https://www.namesilo.com/domain/search-domains?query=${domain}`,
     [Registrar.PORKBUN]: (domain: string) => `https://porkbun.com/checkout/search?q=${domain}`,
 };
