a5f68617386aee84cd3a87e0b202908f3b0de131
Lucio Tudisco <lucio.tudisco@gmail.com>
2025-09-21 18:39:42 +0100

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -47,13 +47,13 @@ const AboutPage = () => {
                 <div className="mx-auto mt-6 max-w-xl lg:mt-14">
                     <FaqItem
                         question="What's a domain hack?"
-                        answer="Domain hacks are clever ways of constructing domain names by combining words with TLDs (Top-Level Domains) to create memorable, eye-catching URLs. Instead of using traditional '.com' domains, these hacks employ extensions like '.ly', '.io', and '.me' to creatively spell out brand names or phrases."
+                        answer="Domain hacks are clever ways of constructing domain names by combining words with TLDs (Top-Level Domains) to to creatively spell out brand names or phrases."
                         color="#fde2e4"
                         index={0}
                     />
                     <FaqItem
                         question="Why are domain hacks so effective?"
-                        answer="Well-crafted domain hacks do more than showcase creativity—they make your site more memorable and shareable. In a sea of standard '.com' addresses, a domain hack can help your website stand out, pique curiosity, and convey a more modern, tech-savvy image."
+                        answer="In a sea of standard '.com' addresses, a domain hack can help your website stand out, pique curiosity, and convey a more modern, tech-savvy image."
                         color="#eff7f6"
                         index={1}
                     />
@@ -71,7 +71,7 @@ const AboutPage = () => {
                     />
                     <FaqItem
                         question="How do I find the perfect domain hack?"
-                        answer="Start by brainstorming words related to your brand or service, then experiment with different TLD combinations. Use our domain search tool to explore creative possibilities and check availability across hundreds of TLDs. Remember, the best domain hacks are short, memorable, and relevant to your brand."
+                        answer="Use our domain search tool to explore creative possibilities and check availability across hundreds of TLDs. Remember, the best domain hacks are short, memorable, and relevant to your brand."
                         color="#bcd4e6"
                         index={4}
                     />
