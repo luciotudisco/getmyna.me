7bfe0efb5cfffe95699a1fc726e9209488f8c26e
luciotudisco <lucio.tudisco@gmail.com>
2025-10-10 07:18:54 +0100

    Handle empty wiki pages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -17,6 +17,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
             priority: 0.8,
         },
         {
+            url: `${baseUrl}/tlds`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
             url: `${baseUrl}/search`,
             lastModified: new Date(),
             changeFrequency: 'daily',
